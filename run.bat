db18052d48436e7227ed470aa6a59b4523e74e37
張小凡 <2672931+whyb@users.noreply.github.com>
2025-07-22 18:19:37 +0800

    Update ffmpeg command

--- run.bat
+++ run.bat
@@ -32,3 +32,9 @@ ffmpeg -y -loop 1 -t 3 -r 1 -i 2.jpg -c:v libx265 -pix_fmt yuv420p10le -tag:v hv
 ffmpeg -y -loop 1 -t 3 -r 1 -i 0_HDR.tif -c:v libx265 -pix_fmt yuv420p10le -x265-params "colorprim=9:transfer=18:colormatrix=9" -tag:v hvc1 -movflags faststart 0_HDR.mov
 
 ffmpeg -y -loop 1 -t 3 -r 1 -i 2_HDR.tif -c:v libx265 -pix_fmt yuv420p10le -x265-params "colorprim=9:transfer=18:colormatrix=9" -tag:v hvc1 -movflags faststart 2_HDR.mov
+
+ffmpeg -i "0_HDR.tif" -frames:v 1 -c:v libaom-av1 -pix_fmt yuv420p10le -qp 32 -vf "setparams=color_primaries=bt2020:transfer_characteristics=smpte2084:colorspace=bt2020nc,setparams=display_mastering=G(0.2650,0.6900)B(0.1500,0.0600)R(0.6800,0.3200)WP(0.3127,0.3290)L(1000.0,0.0001):cll=1000,400" 0_HDR_tmp.avif
+
+ffmpeg -i "0_HDR.tif" -frames:v 1 -c:v libaom-av1 -pix_fmt yuv420p10le -qp 32 -vf "setparams=color_primaries=bt2020:transfer_characteristics=smpte2084:colorspace=bt2020nc" 0_HDR_tmp.avif
+
+ffmpeg -framerate 1 -color_primaries bt2020 -color_trc smpte2084 -colorspace bt2020nc -color_range tv -i 0_HDR.tif -c:v libsvtav1 -pix_fmt yuv420p10le -r 1 -crf 16 -svtav1-params "enable-hdr=1:mastering-display=G(0.265,0.69)B(0.15,0.06)R(0.68,0.32)WP(0.3127,0.329)L(4000.0,0.005):content-light=1000,200:color-primaries=9:transfer-characteristics=16:matrix-coefficients=9" 0_HDR_tmp.avif
